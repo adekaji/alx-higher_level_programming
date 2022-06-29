fab4a2d2b0262fb3ad98c6127456127748c6589e
adekaji <adekaji@gmail.com>
2022-06-29 18:41:00 +0000

    task 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 first_3_letters = word[:3]
 last_2_letters = word[-2:]
 middle_word = word[1:-1]
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+print("First 3 letters: {}".format(first_3_letters))
+print("Last 2 letters: {}".format(last_2_letters))
+print("Middle word: {}".format(middle_word))
