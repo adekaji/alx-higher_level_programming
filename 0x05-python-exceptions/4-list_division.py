831406e7fca209e367aa8bca2db326888e5475df
Godwin Ajiboye <37047475+adekaji@users.noreply.github.com>
2022-07-19 01:53:04 +0100

    Create 4-list_division.py

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides two lists element by element.
+    Args:
+        my_list_1 (list): The first list.
+        my_list_2 (list): The second list.
+        list_length (int): The number of elements to divide.
+    Returns:
+        A new list of length list_length containing all the divisions.
+    """
+    new_list = []
+    for i in range(0, list_length):
+        try:
+            divide_list = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            divide_list = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            divide_list = 0
+        except IndexError:
+            print("out of range")
+            divide_list = 0
+        finally:
+            new_list.append(divide_list)
+    return (new_list)
